d201a906d50d61d099efeb8d9e032ade4efe9786
Alex130220 <dixlofos80@gmail.com>
2026-01-21 01:55:34 +0300

    Initial commit
    Добавлены Django бэкенд и React фронтенд

--- backend/pigeon/views.py
+++ backend/pigeon/views.py
@@ -93,7 +93,7 @@ def get_messages(request):
 def handler404(request, exception):
     return render(request, 'errors/404.html', status=404)
 
-def handler500(request):
+# def handler500(request):
     # return render(request, 'errors/500.html', status=500)
 
 def health_check(request):
